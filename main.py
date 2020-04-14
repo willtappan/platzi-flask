582e79a56485bfd995964d0c1247d2ccd739c1fe
William Tappan <ing.tappan@gmail.com>
2020-04-14 17:47:30 -0500

    FLask-WTF, metodo POST, Desplegar flashes

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
-from flask import Flask , request, make_response , redirect, render_template, session
+from flask import Flask , request, make_response , redirect, render_template, session, url_for, flash
 from flask_bootstrap import Bootstrap
-
+from flask_wtf import FlaskForm
+from wtforms.fields import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 
 app.config['SECRET_KEY']= 'SUPER SECRETO'
 
 todos = ['Comprar CAfe  1', 'Enviar solicitud de compra 2', 'Enviar Producto 3']
+
+class LoginForm(FlaskForm):
+    username = StringField('Nombre del Usuario', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit =SubmitField('Enviar')
 
 @app.errorhandler(404)
 def not_found(error):
@@ -23,17 +30,29 @@ def index():
 
     return response
 
-@app.route('/hello')
+@app.route('/hello', methods=['GET', 'POST'])
 def hello(): 
     
     #user_ip = request.cookies.get('user_ip')
     user_ip = session.get('user_ip')
+    login_form = LoginForm()
+    username = session.get('username')
+
     #return 'HelLo World Platzi , tu ip es {} '.format(user_ip)
     context  = {
             'user_ip':  user_ip,
             'todos' : todos,
-            
+            'login_form': login_form,
+            'username': username,
     }
+
+    if login_form.validate_on_submit():
+        username = login_form.username.data
+        session['username'] = username
+
+        flash ('Nombre del usuario registrado con exito ')
+        return redirect(url_for('index'))
+
     
     return render_template('hello.html', **context)
 
