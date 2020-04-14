0e28ff07f1bae702bc6468d7b77fff9d0929fdec
William Tappan <ing.tappan@gmail.com>
2020-04-13 21:34:01 -0500

    Configuracion de flask

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-from flask import Flask , request, make_response , redirect, render_template
+from flask import Flask , request, make_response , redirect, render_template, session
 from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
+
+app.config['SECRET_KEY']= 'SUPER SECRETO'
 
 todos = ['Comprar CAfe  1', 'Enviar solicitud de compra 2', 'Enviar Producto 3']
 
@@ -16,13 +18,16 @@ def index():
     user_ip = request.remote_addr
     
     response = make_response(redirect('/hello'))
-    response.set_cookie('user_ip', user_ip)
-    
+    #response.set_cookie('user_ip', user_ip)
+    session['user_ip'] = user_ip
+
     return response
 
 @app.route('/hello')
 def hello(): 
-    user_ip = request.cookies.get('user_ip')
+    
+    #user_ip = request.cookies.get('user_ip')
+    user_ip = session.get('user_ip')
     #return 'HelLo World Platzi , tu ip es {} '.format(user_ip)
     context  = {
             'user_ip':  user_ip,
